d0090b4a0ca43f0f40957c41e6829575c843772e
c-strength0911 <57119447+c-strength0911@users.noreply.github.com>
2021-06-12 14:52:53 +0900

    file 업로드 라우터

---
+++ routers/fileRouter.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const uploadFile = require("../middlewares/fileUpload");
+
+const pool = require("../config/poolConfig.js");
+const router = express.Router();
+
+router.post("/:fileType", uploadFile, async (req, res) => {
+  const { file } = req;
+  const fileUrl = file.path;
+  const fileType = req.params.fileType;
+  const fileExtension = file.mimetype.split("/")[1];
+  let status = "002"; // 임시
+  try {
+    const [result] = await pool.query(QUERY.FILE.INSERT, [
+      fileExtension,
+      fileUrl,
+      fileType,
+      status,
+    ]);
+    console.log(result);
+    console.log("routing is working");
+    res.status(200).json({ result: "success", fileNo: result.insertId });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ result: "false", errorMsg: error });
+  }
+});
+
+module.exports = router;
