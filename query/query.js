feb8e320f9e9cf65a39443b8c644b4da068d891e
c-strength0911 <57119447+c-strength0911@users.noreply.github.com>
2021-06-12 14:54:33 +0900

    file insert 추가

--- query/query.js
+++ query/query.js
@@ -1,1 +1,16 @@
-module.exports = {};
+module.exports = {
+  USER: {
+    INSERT: `INSERT 
+                INTO user(user_id, user_password, user_nickname, user_type) 
+                VALUE(?,?,?,?)`,
+    SELECT_BY_ID: `SELCET * 
+                      FROM user 
+                      WHERE user_id=? 
+                  AND user_password=?`,
+  },
+  FILE: {
+    INSERT: `INSERT
+  INTO file(file_ext, file_url, file_type, file_status)
+  VALUES(?,?,?,?)`,
+  },
+};
