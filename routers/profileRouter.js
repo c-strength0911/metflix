6c00776b4f08436c04e4865d6d43b2289486d60d
strength C_ <c.strength0911@gmail.com>
2021-07-30 04:54:50 +0000

    profile

--- routers/profileRouter.js
+++ routers/profileRouter.js
@@ -9,7 +9,7 @@ router.get("/profile", validator.body(profileSchema), async (req, res) => {
     if (!req.session.userNo) {
       res.render("/login");
     }
-	  await 
+	  const result = await pool.query("SELECT * FROM user LEFT JOIN file ON user.user_profile_file_no = file.file_no UNION SELECT * FROM user RIGHT JOIN file ON user.user_profile_file_no = file.file_no")
   } catch (error){
 	console.error(error);
   }
