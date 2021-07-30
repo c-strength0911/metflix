895b0ea7dfc8ae84fbc8aa231e8707eda5c8d4c0
strength C_ <c.strength0911@gmail.com>
2021-07-30 06:14:04 +0000

    profile 쿼리 추가

--- routers/profileRouter.js
+++ routers/profileRouter.js
@@ -5,13 +5,14 @@ const router = express.Router();
 const validator = require("express-joi-validation").createValidator({});
 const { profileSchema } = require("../validations/userValidater");
 router.get("/profile", validator.body(profileSchema), async (req, res) => {
+	const userId = req.body;
   try {
     if (!req.session.userNo) {
       res.render("/login");
     }
-	  const result = await pool.query("SELECT * FROM user LEFT JOIN file ON user.user_profile_file_no = file.file_no UNION SELECT * FROM user RIGHT JOIN file ON user.user_profile_file_no = file.file_no")
+// 	  쿼리 재확인 필요 (SELECT * FROM 수정) 
+	  const result = await pool.query("SELECT * FROM user WHERE user_id=? LEFT JOIN file ON user.user_profile_file_no = file.file_no UNION SELECT * FROM user WHERE user_id=? RIGHT JOIN file ON user.user_profile_file_no = file.file_no", userId )
   } catch (error){
 	console.error(error);
   }
-	
 });
